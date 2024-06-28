3d15c8e50aa67b362127e94c97690998fb1792af
Saumya Chandrakant Prasad <100471966+saumya07p@users.noreply.github.com>
2024-06-28 21:07:20 +0530

    Add files via upload
    Added the database file

---
+++ group4.sql
@@ -0,0 +1,6 @@
+INSERT INTO Warehousee (warehouseid, emp_id, name, capacity) VALUES
+(1, 'mgr001', 'A', 250),
+(2, 'wrk011', 'B', 275),
+(3, 'wrk002', 'C', 300),
+(4, 'wrk007', 'D', 325),
+(5, 'wrk016', 'E', 350);
